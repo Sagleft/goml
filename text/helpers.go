bb5277f382e63422b90cd0d1af234cfa0bd0aea9
Sagleft <sagleft@mfc.click>
2026-02-28 04:57:22 +0300

    add gzip support

---
+++ text/helpers.go
@@ -0,0 +1,34 @@
+package text
+
+import (
+	"bytes"
+	"compress/gzip"
+	"fmt"
+	"io"
+)
+
+func compressJsonBytes(jsonBytes []byte) ([]byte, error) {
+	var buf bytes.Buffer
+	gz := gzip.NewWriter(&buf)
+
+	if _, err := gz.Write(jsonBytes); err != nil {
+		return nil, fmt.Errorf("write: %w", err)
+	}
+	defer gz.Close()
+
+	return buf.Bytes(), nil
+}
+
+func decompressJsonBytes(gzBytes []byte) ([]byte, error) {
+	r, err := gzip.NewReader(bytes.NewReader(gzBytes))
+	if err != nil {
+		return nil, fmt.Errorf("create reader: %w", err)
+	}
+	defer r.Close()
+
+	decompressed, err := io.ReadAll(r)
+	if err != nil {
+		return nil, fmt.Errorf("read all: %w", err)
+	}
+	return decompressed, nil
+}
